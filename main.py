174405123499aa65df0ad2718e6eb2f47a7a0e79
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-13 22:34:26 +0100

    Added the 3 types of variables

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ __winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
 __human_name__ = 'variables'
 
 # Add your code after this line
+my_string = 'This is an example of a string'
+my_integer = 1977
+my_boolean = True
